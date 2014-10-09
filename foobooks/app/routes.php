654d8811a0bdabf3ea44801b822378e243906fab
masbaty <masbaty@gmail.com>
2014-10-08 23:21:03 -0400

    Messed around with routes.php to learn about routing. Also installed Pre without errors by specifying all versions of Laravel 4.

--- foobooks/app/routes.php
+++ foobooks/app/routes.php
@@ -13,10 +13,32 @@
 
 Route::get('/', function()
 {
-	return "Hello World!";
-	//return View::make('hello');
+	//return "Hello World!";
+	return View::make('hello');
 });
 
 Route::get("/saygoodbye", function() {
 	return "Goodbye World!";
+});
+
+
+Route::get("/new", function() {
+	$view  = '<form method="POST" action="/new">';
+	$view .= 'Title: <input type="text" name="title">';
+	$view .= '<input type="submit">';
+	return $view;
+});
+
+Route::post("/new", function() {
+	$input = Input::all();
+	print_r($input);
+});
+
+
+Route::get('/books', function() {
+	return "Here are all the books...";
+});
+
+Route::get('/books/{category}', function($category) {	// Category is a parameter. User will decide category.
+	return "Here are the books in the category: ".$category;
 });
